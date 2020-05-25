4ac62755d193a3461c1351b818389ed2ec4e3d4a
Soraya Valle <svalleortega@gmail.com>
2020-05-25 12:11:45 +0200

    Add grid template and first js structure

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const getRandomLetter = () => {
+  const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let randomLetter = letters[Math.ceil(Math.random() * letters.length)];
+}
